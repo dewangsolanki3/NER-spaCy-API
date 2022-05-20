490f0eb8e4205ef3294b7b74c39feb113df6d051
Vaishnav Bharadwaj <vaishnavbharadwaj@Vaishnavs-MacBook-Air.local>
2022-05-20 02:17:41 +0100

    code clearing

--- main.py
+++ main.py
@@ -27,14 +27,21 @@ def test():
 def ner():
     dict_sub.clear()
     ner_model = spacy.load('ner_model')
+    data = request.get_json()
+    text_data = data['data']
 
-    file = 'Dewang Solanki CV.pdf'
-    docs = fitz.open(file)
-    my_cv = ""
-    for doc in docs:
-        my_cv = my_cv + str(doc.get_text())
+    # done after upload file
+    # file = arg.get('pdf')
+    # docs = fitz.open(file)
+    # my_cv = ""
+    # for doc in docs:
+    #     my_cv = my_cv + str(doc.get_text())
+    # text_json = {'data': my_cv}
+    # pdf_json = json.dumps(text_json)
+    # print(pdf_json)
 
-    doc = ner_model(" ".join(my_cv.split('\n')))
+    # my_cv = ''
+    doc = ner_model(" ".join(text_data.split('\n')))
     for entities in doc.ents:
         val_append(dict_sub, entities.label_.upper(), entities.text)
     return dict_sub
